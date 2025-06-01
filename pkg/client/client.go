3c9fb52e9bb772c49c7dc52d8f8c5eb40b996de4
Thijs Haker <67151465+TheDevtop@users.noreply.github.com>
2025-06-01 17:31:54 +0200

    Merged client

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,1 +1,33 @@
 package client
+
+import (
+	"errors"
+	"io"
+	"net/http"
+
+	"github.com/TheDevtop/quicktable/pkg/shared"
+)
+
+func formatPath(base string, route string) string {
+	return base + route
+}
+
+func Open(addr string) (*Conn, error) {
+	var (
+		resp *http.Response
+		err  error
+		buf  []byte
+		dbc  = new(Conn)
+	)
+	if resp, err = http.Get(formatPath(addr, shared.RoutePing)); err != nil {
+		return nil, err
+	}
+	if buf, err = io.ReadAll(resp.Body); err != nil {
+		return nil, err
+	}
+	if string(buf) != "\"pong!\"" {
+		return nil, errors.New("Could not connect to Quicktable")
+	}
+	dbc.baseAddr = addr
+	return dbc, nil
+}
