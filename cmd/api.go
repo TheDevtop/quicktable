27fa3baba956f398af4eb08ef7d395f7bb0d079a
Thijs Haker <theage2001@gmail.com>
2026-06-29 15:44:00 +0200

    Implemented apiExact*() functions

--- cmd/api.go
+++ cmd/api.go
@@ -56,7 +56,36 @@ func apiIndexPrefix(w http.ResponseWriter, r *http.Request) {
 }
 
 func apiQueryExact(w http.ResponseWriter, r *http.Request) {
+	var (
+		err  error
+		form api.FormExact
+	)
 
+	if form, err = api.DecodeStream[api.FormExact](r.Body); err != nil {
+		logPtr.Error("apiQueryExact", "err", err)
+		api.EncodeStream(w, api.FormResponse[string]{
+			Route:  api.RouteQueryExact,
+			Status: api.StatusApiError,
+			Data:   err.Error(),
+		})
+		return
+	}
+
+	if form.Value, err = engine.QueryExact(form.Key); err != nil {
+		logPtr.Error("apiQueryExact", "err", err)
+		api.EncodeStream(w, api.FormResponse[string]{
+			Route:  api.RouteQueryExact,
+			Status: api.StatusEngineError,
+			Data:   err.Error(),
+		})
+		return
+	}
+
+	api.EncodeStream(w, api.FormResponse[string]{
+		Route:  api.RouteQueryExact,
+		Status: api.StatusOk,
+		Data:   form.Value,
+	})
 }
 
 func apiQueryPrefix(w http.ResponseWriter, r *http.Request) {
@@ -64,7 +93,36 @@ func apiQueryPrefix(w http.ResponseWriter, r *http.Request) {
 }
 
 func apiInsertExact(w http.ResponseWriter, r *http.Request) {
+	var (
+		err  error
+		form api.FormExact
+	)
 
+	if form, err = api.DecodeStream[api.FormExact](r.Body); err != nil {
+		logPtr.Error("apiInsertExact", "err", err)
+		api.EncodeStream(w, api.FormResponse[string]{
+			Route:  api.RouteInsertExact,
+			Status: api.StatusApiError,
+			Data:   err.Error(),
+		})
+		return
+	}
+
+	if form.Key, err = engine.InsertExact(form.Key, form.Value); err != nil {
+		logPtr.Error("apiInsertExact", "err", err)
+		api.EncodeStream(w, api.FormResponse[string]{
+			Route:  api.RouteInsertExact,
+			Status: api.StatusEngineError,
+			Data:   err.Error(),
+		})
+		return
+	}
+
+	api.EncodeStream(w, api.FormResponse[string]{
+		Route:  api.RouteInsertExact,
+		Status: api.StatusOk,
+		Data:   form.Key,
+	})
 }
 
 func apiInsertPrefix(w http.ResponseWriter, r *http.Request) {
@@ -72,7 +130,36 @@ func apiInsertPrefix(w http.ResponseWriter, r *http.Request) {
 }
 
 func apiDeleteExact(w http.ResponseWriter, r *http.Request) {
+	var (
+		err  error
+		form api.FormExact
+	)
 
+	if form, err = api.DecodeStream[api.FormExact](r.Body); err != nil {
+		logPtr.Error("apiDeleteExact", "err", err)
+		api.EncodeStream(w, api.FormResponse[string]{
+			Route:  api.RouteDeleteExact,
+			Status: api.StatusApiError,
+			Data:   err.Error(),
+		})
+		return
+	}
+
+	if form.Key, err = engine.DeleteExact(form.Key); err != nil {
+		logPtr.Error("apiDeleteExact", "err", err)
+		api.EncodeStream(w, api.FormResponse[string]{
+			Route:  api.RouteDeleteExact,
+			Status: api.StatusEngineError,
+			Data:   err.Error(),
+		})
+		return
+	}
+
+	api.EncodeStream(w, api.FormResponse[string]{
+		Route:  api.RouteDeleteExact,
+		Status: api.StatusOk,
+		Data:   form.Key,
+	})
 }
 
 func apiDeletePrefix(w http.ResponseWriter, r *http.Request) {
